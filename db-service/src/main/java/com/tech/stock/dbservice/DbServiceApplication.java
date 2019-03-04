57479bbab2cc67da231a57cd6ae31fb83607af7d
naveenperera777 <naveenperera777@gmail.com>
2019-03-04 14:32:03 +0530

    Add service registry and Stock service

--- db-service/src/main/java/com/tech/stock/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/tech/stock/dbservice/DbServiceApplication.java
@@ -2,8 +2,10 @@ package com.tech.stock.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.tech.stock.dbservice.repository" )
 @SpringBootApplication
 public class  DbServiceApplication {
